b53b9a4300f5cd4505c4096bc614106fc5aeb3d2
Anas kotb <140127193+anaskotb@users.noreply.github.com>
2024-07-17 00:39:46 +0300

    Task1

---
+++ Python/Session1/Task1.py
@@ -0,0 +1,18 @@
+'''
+Write a Python program to count the number 4 in a given list.
+'''
+l=[1,4,6,7,4,2]
+print(l.count(4))
+'''
+
+Write a Python program to test whether a passed letter is a vowel or not.
+'''
+V="AEIOUaeiou"
+userinput=input("please enter the letter")
+
+if userinput in V:
+        print("True")
+else:
+     print("False")
+
+
